a2bdf73b8f577acd746e5040385b14abe2bf01ad
An Qin <anqin.qin@gmail.com>
2018-04-25 14:46:10 +0800

    add the support codes

---
+++ BUILD
@@ -0,0 +1,7 @@
+cc_library(
+    name = 'xsheet_flags',
+    srcs = 'xsheet_flags.cc',
+    deps = [
+        'thirdparty/glfags:gflags'
+    ]
+)
---
+++ xsheet_flags.cc
@@ -0,0 +1,13 @@
+// Copyright (C) 2018, For GeekerClub authors.
+// Author: An Qin (anqin.qin@gmail.com)
+//
+// Description:
+
+
+#include "thirdparty/gflags/gflags.h"
+
+namespace xsheet {
+
+DEFINE_string(xsheet_leveldb_env_type, "local", "base env for leveldb");
+
+} // namespace xsheet
