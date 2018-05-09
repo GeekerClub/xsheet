dd3f4e5d88f9a9f17950764b4c7fd803ca726d45
An Qin <anqin.qin@gmail.com>
2018-05-09 21:48:54 +0800

    read impl framework

---
+++ sdk/read_impl.h
@@ -0,0 +1,22 @@
+// Copyright (C) 2018, For GeekerClub authors.
+// Author: An Qin (anqin.qin@gmail.com)
+//
+// Description:
+
+
+#ifndef XSHEE_SDK_READ_IMPL_H
+#define XSHEE_SDK_READ_IMPL_H
+
+
+namespace xsheet {
+
+class ReadImpl : public Reader {
+public:
+    ReadImpl();
+    ~ReadImpl();
+
+};
+
+} // namespace xsheet
+
+#endif // XSHEE_SDK_READ_IMPL_H
