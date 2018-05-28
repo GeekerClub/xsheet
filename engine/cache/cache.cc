2a43f505dfa574b40b4db129a8cbf678edf7ba94
An Qin <anqin.qin@gmail.com>
2018-05-28 17:10:09 +0800

    cache base system

---
+++ engine/cache/cache.cc
@@ -0,0 +1,53 @@
+// Copyright (C) 2018, For GeekerClub authors.
+// Author: An Qin (anqin.qin@gmail.com)
+//
+// Description:
+
+#include "engine/cache/cache.h"
+
+
+namespace xsheet {
+
+Cache::Cache(const std::string& name, CacheOptions options)
+    : name_(name), options_(options) {}
+
+CacheSystem* Cache::GetCacheSystemByName(const std::string& db_path, std::string* real_path) {
+    // "/hot_predict/abc" -> "hot_predict"
+    if (db_path[0] == '/') {
+        size_t next_slash = db_path.find('/', 1);
+        if (next_slash != std::string::npos) {
+            std::string prefix = db_path.substr(1, next_slash - 1);
+            *real_path = db_path.substr(next_slash + 1, db_path.length() - 1);
+            CacheSystem* cs = TOFT_GET_CACHE_SYSTEM(prefix);
+            if (fs != NULL)
+                return fs;
+        }
+    }
+    return TOFT_GET_CACHE_SYSTEM("default");
+}
+
+Cache* Cache::Open(const std::string& db_path, const CacheOptions& options) {
+    std::string real_path = db_path;
+    CacheSystem* cs = GetCacheSystemByName(db_path, &real_path);
+    return cs->Open(real_path, options);
+}
+
+bool Cache::Exists(const std::string& db_path) {
+    std::string real_path = db_path;
+    CacheSystem* cs = GetCacheSystemByName(db_path);
+    return cs->Exists(real_path);
+}
+
+bool Cache::Delete(const std::string& db_path) {
+    std::string real_path = db_path;
+    CacheSystem* cs = GetCacheSystemByName(db_path);
+    return cs->Delete(real_path);
+}
+
+int64_t Cache::GetSize(const std::string& db_path) {
+    std::string real_path = db_path;
+    CacheSystem* cs = GetCacheSystemByName(db_path);
+    return cs->GetSize(real_path);
+}
+
+} // namespace xsheet
---
+++ engine/cache/cache.h
@@ -0,0 +1,77 @@
+// Copyright (C) 2018, For GeekerClub authors.
+// Author: An Qin (anqin.qin@gmail.com)
+//
+// Description:
+
+
+#ifndef XSHEET_ENGINE_CACHE_CACHE_H
+#define XSHEET_ENGINE_CACHE_CACHE_H
+
+
+namespace xsheet {
+
+struct CacheOptions {
+
+};
+
+class Cache {
+public:
+    typedef void (*)(const toft::StringPiece& key, const toft::StringPiece& value) Handle;
+    struct Result {};
+
+public:
+    Cache(const std::string& name, CacheOptions options);
+    virtual ~Cache() {}
+
+    virtual toft::StringPiece Value() = 0;
+
+    virtual Resutl* Insert(const toft::StringPiece& key,
+                           const toft::StringPiece& value) = 0;
+    virtual Result* Erase(const toft::StringPiece& key, Handle handle) = 0;
+
+public:
+    static Cache* Open(const std::string& name, const CacheOptions& options);
+    static bool Exists(const std::string& name);
+    static  bool Delete(const std::string& name);
+    static int64_t GetSize(const std::string& name);
+
+protected:
+    static CacheSystem* GetCacheSystemByName(const std::string& name,
+                                             std::string* real_path = NULL);
+protected:
+    std::string name_;
+    CacheOptions options_;
+};
+
+
+class CacheSystem {
+public:
+    CacheSystem() {}
+    virtual ~CacheSystem() {}
+
+    virtual Cache* Open(const std::string& path, const CacheOptions& options) = 0;
+    virtual bool Exists(const std::string& path) = 0;
+    virtual bool Delete(const std::string& path) = 0;
+    virtual int64_t GetSize(const std::string& path) = 0;
+};
+
+TOFT_CLASS_REGISTRY_DEFINE_SINGLETON(cache_system, CacheSystem);
+
+} // namespace xsheet
+
+
+#define TOFT_REGISTER_CACHE_SYSTEM(prefix, class_name) \
+    TOFT_CLASS_REGISTRY_REGISTER_CLASS_SINGLETON( \
+        cache_system, CacheSystem, prefix, class_name)
+
+// Get CacheSystem singleton from prefix.
+#define TOFT_GET_CACHE_SYSTEM(prefix) \
+    TOFT_CLASS_REGISTRY_GET_SINGLETON(cache_system, prefix)
+
+// Count of registed database systems.
+#define TOFT_CACHE_SYSTEM_COUNT() TOFT_CLASS_REGISTRY_CLASS_COUNT(cache_system)
+
+// Get database systems name by index
+#define TOFT_CACHE_SYSTEM_NAME(i) TOFT_CLASS_REGISTRY_CLASS_NAME(cache_system, i)
+
+#endif // XSHEET_ENGINE_CACHE_CACHE_H
