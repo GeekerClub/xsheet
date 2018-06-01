f9c705bd1d34ea93f1a17a012742201b77fb45d3
An Qin <anqin.qin@gmail.com>
2018-06-01 14:41:45 +0800

    unit test for hr cache

---
+++ engine/cache/hr_cache_test.cc
@@ -0,0 +1,25 @@
+// Copyright (C) 2018, For GeekerClub authors.
+// Author: An Qin (anqin.qin@gmail.com)
+//
+// Description:
+
+#include "thirdparty/gflags/gflags.h"
+#include "thirdparty/glog/logging.h"
+#include "thirdparty/gtest/gtest.h"
+
+namespace xsheet {
+
+class HRCacheTest : public ::testing::Test {
+public:
+    HRCacheTest() {}
+    ~HRCacheTest() {}
+
+protected:
+
+};
+
+TEST_F(HRCacheTest, General) {
+
+}
+
+} // namespace xsheet
