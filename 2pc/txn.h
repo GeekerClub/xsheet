578ff3f2bb2ab5f2b31d5bd49f53dfe9155fdc16
An Qin <anqin.qin@gmail.com>
2018-07-02 20:10:26 +0800

    tranasaction interfaces

---
+++ 2pc/txn.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2018, For GeekerClub authors.
+// Author: An Qin (anqin.qin@gmail.com)
+//
+// Description:
+
+#ifndef XSHEET_TXN_TXN_H
+#define XSHEET_TXN_TXN_H
+
+class Txn {
+
+};
+
+#endif // XSHEET_TXN_TXN_H
