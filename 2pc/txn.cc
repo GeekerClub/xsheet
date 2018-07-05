4d2fcc2f9b51fe1a816fd76556f03c4f211545eb
An Qin <anqin.qin@gmail.com>
2018-07-05 21:32:20 +0800

    txn

---
+++ 2pc/txn.cc
@@ -0,0 +1,11 @@
+// Copyright (C) 2018, For GeekerClub authors.
+// Author: An Qin (anqin.qin@gmail.com)
+//
+// Description:
+
+
+#include "2pc/txn.h"
+
+namespace xsheet {
+
+} // namespace xsheet
